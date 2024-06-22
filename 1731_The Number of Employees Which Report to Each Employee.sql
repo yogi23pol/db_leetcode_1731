83cf027f1047e8d37cfa57195a3830bc8ec85efd
Yogesh Pol <132032441+yogi23pol@users.noreply.github.com>
2024-06-22 19:33:45 +0530

    Query

--- 1731_The Number of Employees Which Report to Each Employee.sql
+++ 1731_The Number of Employees Which Report to Each Employee.sql
@@ -38,3 +38,9 @@ Output:
 +-------------+-------+---------------+-------------+
 Explanation: Hercy has 2 people report directly to him, Alice and Bob. Their average age is (41+36)/2 = 38.5, which is 39 after rounding it to the nearest integer.
 */
+
+SELECT m.employee_id, m.name, COUNT(e.employee_id) AS reports_count, ROUND(AVG(e.age)) AS average_age 
+FROM Employees_42 AS m
+JOIN Employees_42 AS e
+ON m.employee_id = e.reports_to
+GROUP BY m.employee_id, m.name;
